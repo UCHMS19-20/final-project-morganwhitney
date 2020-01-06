540195ea566ca4e66ee8d86b88327493df552127
morgancwhitney <57999154+morgancwhitney@users.noreply.github.com>
2020-01-06 10:21:24 -0500

    Update mainthingy.py
    continued outining each individual event, most of the way through the day.

--- mainthingy.py
+++ mainthingy.py
@@ -12,6 +12,58 @@ def wakeup():
         print("We both know that never works. You overslept and missed your bus. Since mom is at work already, you have to stay home today and miss your physics test.")
         sys.exit
     if user_choice_w == 1:
+        print("You hit snooze")
 
+def transportation(result):
+    if result == 
+
+def school_arrival():
+    #auditorium
+        #do hw
+            -90 on pop quiz, best option
+        #talk to vanessa
+            #neutral option, get 80 on pop quiz
+        #nap
+            #groggy, gonna fall asleep in sans, 0 quiz
+
+def sans():
+    -post nap
+        -go to nurse after 0 on quiz
+        -stay in nurse until co, then go home
+        -no practice, overall bad day
+    -neutral option 
+        -skate through w nothing special, its hstory
+    - best option
+        -90 quiz, but tsill its sans nothing exciting
+
+def tech ():
+    -do work
+        -best option 
+        -put in headphones n jam to mcr
+        -productivity
+        -allude to the making of this very project bc thats funny
+    -hang out
+        -more fun option
+        -you needed this break sans was stressful
+        -you can feel your unfinished final project staring into
+         your soul, telling you youre gonna fail
     
 
+def physics():
+    -fall asleep
+        -you black out 2 minutes into class 
+    -take notes
+        -slow class
+        -you feel prepared bc you actually know whats going on
+        -okay option
+    -daydream
+        -class goes by quickly, you think about practice and ypur plans
+
+def math():
+
+def dinner():
+
+def practice():
+    def hospital_visit():
+
+def bedtime():
